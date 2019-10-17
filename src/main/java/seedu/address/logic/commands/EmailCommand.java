ce26bd3fef05e45a304a06d0aee6918f74abd7b1
Zhong Jun <zhongjun@jorked.com>
2019-10-17 18:13:19 +0800

    Remove unused import

--- src/main/java/seedu/address/logic/commands/EmailCommand.java
+++ src/main/java/seedu/address/logic/commands/EmailCommand.java
@@ -2,7 +2,6 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
-import java.io.IOException;
 import java.util.List;
 
 import seedu.address.commons.core.Messages;
