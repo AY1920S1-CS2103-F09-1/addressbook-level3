789fa9fdfdf07682844b523aa0465ead3ba46174
Chris Kheng <chriskheng1609@gmail.com>
2019-10-20 20:54:55 +0800

    Separate column titles from Schedule's observable list

--- src/main/java/seedu/address/commons/util/Pair.java
+++ src/main/java/seedu/address/commons/util/Pair.java
@@ -4,5 +4,21 @@ public class Pair<H, T> {
     private H head;
     private T tail;
 
-    public Pair()
+    public Pair(H head, T tail) {
+        this.head = head;
+        this.tail = tail;
+    }
+
+    public H getHead() {
+        return head;
+    }
+
+    public T getTail() {
+        return tail;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%s, %s)", head, tail);
+    }
 }
