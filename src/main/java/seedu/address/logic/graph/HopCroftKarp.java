225e6f7493d36353b77351fc03835c284a856bcd
Chris Kheng <chriskheng1609@gmail.com>
2019-10-30 16:37:23 +0800

    Implement HopCroftKarp algorithm

--- src/main/java/seedu/address/logic/graph/HopCroftKarp.java
+++ src/main/java/seedu/address/logic/graph/HopCroftKarp.java
@@ -1,8 +1,11 @@
 package seedu.address.logic.graph;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 /**
  * Runs the Hopcroft-Karp algorithm on the given bipartite graphs of interviewee and available interview slots
@@ -10,28 +13,47 @@ import java.util.List;
  * Crucial assumption: A bipartite graph is given to the algorithm.
  */
 public class HopCroftKarp {
-    private List<List<InterviewSlotVertex>> intervieweePredecessors;
+    private BipartiteGraph graph;
+    private List<InterviewSlotVertex> intervieweePredecessor;
     private List<List<IntervieweeVertex>> interviewSlotPredecessors;
     private boolean[] usedInterviewees;
     private boolean[] usedSlots;
 
-    public void execute(BipartiteGraph graph) {
+    public HopCroftKarp(BipartiteGraph graph) {
+        this.graph = graph;
+    }
+
+    public void execute() {
+        initialiseHopCroftKarp();
+        List<InterviewSlotVertex> lastLayer = new LinkedList<>();
+
+        do {
+            lastLayer = new BfsHopCroftKarp(graph).execute(intervieweePredecessor, interviewSlotPredecessors);
+            // If augmenting path(s) is found
+            if (!lastLayer.isEmpty()) {
+                new DfsHopCroftKarp(graph).execute(lastLayer, intervieweePredecessor, interviewSlotPredecessors,
+                    usedInterviewees, usedSlots);
+            }
+            cleanUp();
+        } while (!lastLayer.isEmpty()); // while there exists an augmenting path(s)
+    }
+
+    private void initialiseHopCroftKarp() {
         int numInterviewees = graph.getNumInterviewees();
         int numSlots = graph.getNumInterviewSlots();
 
-        // NEED TO CLEAN UP THESE 4 STUFF after every iteration
-        intervieweePredecessors = new ArrayList<>(numInterviewees);
+        intervieweePredecessor = Arrays.asList(new InterviewSlotVertex[numInterviewees]);
         interviewSlotPredecessors = new ArrayList<>(numSlots);
         usedInterviewees = new boolean[numInterviewees];
         usedSlots = new boolean[numSlots];
 
-        fillWithSubLists(intervieweePredecessors, numInterviewees);
-        fillWithSubLists(interviewSlotPredecessors, numSlots);
+        IntStream.range(0, numSlots).forEach(i -> interviewSlotPredecessors.add(new LinkedList<>()));
     }
 
-    private <Z> void fillWithSubLists(List<List<Z>> list, int numInterviewees) {
-        for (int i = 0; i < numInterviewees; i++) {
-            list.add(new LinkedList<>());
-        }
+    private void cleanUp() {
+        Collections.fill(intervieweePredecessor, null);
+        interviewSlotPredecessors.forEach(list -> list.clear());
+        Arrays.fill(usedInterviewees, false);
+        Arrays.fill(usedSlots, false);
     }
 }
--- src/test/java/seedu/address/logic/graph/HopCroftKarpTest.java
+++ src/test/java/seedu/address/logic/graph/HopCroftKarpTest.java
@@ -1,5 +1,24 @@
-import static org.junit.jupiter.api.Assertions.*;
+package seedu.address.logic.graph;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.testutil.SampleGraph;
 
 class HopCroftKarpTest {
+    @Test
+    public void execute_sampleGraph1_allMatched() {
+        BipartiteGraph subjectGraph = SampleGraph.getSampleGraphOne();
+        new HopCroftKarp(subjectGraph).execute();
+        int numInterviewees = subjectGraph.getNumInterviewees();
 
+        IntStream.range(0, numInterviewees).forEach(i -> {
+            IntervieweeVertex intervieweeVertex = subjectGraph.getIntervieweePair(i).getHead();
+            assert intervieweeVertex.isMatched() : fail("Something is wrong!");
+            // System.out.println(intervieweeVertex.getPartner().getItem()); // TODO: comment this out!
+        });
+    }
 }
